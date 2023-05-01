9f122eef84f6fa1bb36f9b487edec6c2f9f38ac3
BestLegendYT <80245774+BestLegendYT@users.noreply.github.com>
2023-05-01 16:02:29 +0300

    Update Skoot.lua

--- Skoot.lua
+++ Skoot.lua
@@ -18,15 +18,15 @@ ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
 Frame.Parent = ScreenGui
 Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Frame.Position = UDim2.new(0.347368419, 0, 0.383529425, 0)
-Frame.Size = UDim2.new(0.411740869, 0, 0.21294117, 0)
+Frame.Position = UDim2.new(0.325506091, 0, 0.384705842, 0)
+Frame.Size = UDim2.new(0.348582953, 0, 0.24823527, 0)
 
 ScrollingFrame.Parent = Frame
 ScrollingFrame.Active = true
 ScrollingFrame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 ScrollingFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-ScrollingFrame.Position = UDim2.new(0.00795233529, 0, 0.0514624156, 0)
-ScrollingFrame.Size = UDim2.new(0, 496, 0, 165)
+ScrollingFrame.Position = UDim2.new(0.0233035479, 0, 0.0392260477, 0)
+ScrollingFrame.Size = UDim2.new(0.959390104, 0, 0.906629503, 0)
 
 TextButton.Parent = ScrollingFrame
 TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -42,17 +42,18 @@ UICorner_2.Parent = Frame
 
 TextButton_2.Parent = ScreenGui
 TextButton_2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_2.Position = UDim2.new(0.0153846154, 0, 0.479999989, 0)
-TextButton_2.Size = UDim2.new(0, 200, 0, 50)
+TextButton_2.Position = UDim2.new(0.958032846, 0, 0.148329347, 0)
+TextButton_2.Size = UDim2.new(0.0417004041, 0, 0.119999997, 0)
 TextButton_2.Font = Enum.Font.SourceSans
 TextButton_2.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextButton_2.TextSize = 14.000
 
+UICorner_3.CornerRadius = UDim.new(1, 8)
 UICorner_3.Parent = TextButton_2
 
 -- Scripts:
 
-local function CFJYVB_fake_script() -- Frame.LocalScript 
+local function SEUOV_fake_script() -- Frame.LocalScript 
 	local script = Instance.new('LocalScript', Frame)
 
 	local UIS = game:GetService('UserInputService')
@@ -61,14 +62,14 @@ local function CFJYVB_fake_script() -- Frame.LocalScript
 	local dragSpeed = 0.25
 	local dragStart = nil
 	local startPos = nil
-
+	
 	local function updateInput(input)
 		local delta = input.Position - dragStart
 		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
 			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
 		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
 	end
-
+	
 	frame.InputBegan:Connect(function(input)
 		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
 			dragToggle = true
@@ -81,7 +82,7 @@ local function CFJYVB_fake_script() -- Frame.LocalScript
 			end)
 		end
 	end)
-
+	
 	UIS.InputChanged:Connect(function(input)
 		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
 			if dragToggle then
@@ -90,16 +91,16 @@ local function CFJYVB_fake_script() -- Frame.LocalScript
 		end
 	end)
 end
-coroutine.wrap(CFJYVB_fake_script)()
-local function RFFRGB_fake_script() -- ScreenGui.LocalScript 
+coroutine.wrap(SEUOV_fake_script)()
+local function YKAI_fake_script() -- ScreenGui.LocalScript 
 	local script = Instance.new('LocalScript', ScreenGui)
 
 	local SGui = script.Parent
 	local Frame = SGui:WaitForChild("Frame")
 	local Button = SGui:WaitForChild("TextButton")
-
+	
 	Button.MouseButton1Up:Connect(function()
 		Frame.Visible = not Frame.Visible
 	end)
 end
-coroutine.wrap(RFFRGB_fake_script)()
+coroutine.wrap(YKAI_fake_script)()
