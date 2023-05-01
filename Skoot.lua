c1802c6d0bebbe3b5ac79c1e28b77d38a146ec60
BestLegendYT <80245774+BestLegendYT@users.noreply.github.com>
2023-05-01 15:38:22 +0300

    Update 0.1
    Fixed Some Bugs and Added a Test Script

--- Skoot.lua
+++ Skoot.lua
@@ -3,56 +3,46 @@
 
 -- Instances:
 
-local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local ScrollingFrame = Instance.new("ScrollingFrame")
-local TextButton = Instance.new("TextButton")
+local Fly = Instance.new("TextButton")
 local UICorner = Instance.new("UICorner")
 local UICorner_2 = Instance.new("UICorner")
-local TextButton_2 = Instance.new("TextButton")
-local UICorner_3 = Instance.new("UICorner")
 
 --Properties:
 
-ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-
-Frame.Parent = ScreenGui
+Frame.Parent = game.StarterGui.ScreenGui
 Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Frame.Position = UDim2.new(0.347368419, 0, 0.383529425, 0)
-Frame.Size = UDim2.new(0.411740869, 0, 0.21294117, 0)
+Frame.Position = UDim2.new(0.325506091, 0, 0.384705842, 0)
+Frame.Size = UDim2.new(0.348582953, 0, 0.24823527, 0)
 
 ScrollingFrame.Parent = Frame
 ScrollingFrame.Active = true
 ScrollingFrame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 ScrollingFrame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-ScrollingFrame.Position = UDim2.new(0.00795233529, 0, 0.0514624156, 0)
-ScrollingFrame.Size = UDim2.new(0, 496, 0, 165)
+ScrollingFrame.Position = UDim2.new(0.0233035479, 0, 0.0392260477, 0)
+ScrollingFrame.Size = UDim2.new(0.959390104, 0, 0.906629503, 0)
 
-TextButton.Parent = ScrollingFrame
-TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextButton.Position = UDim2.new(0.0182186235, 0, 0.0172198527, 0)
-TextButton.Size = UDim2.new(0, 138, 0, 31)
-TextButton.Font = Enum.Font.SourceSans
-TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton.TextSize = 14.000
+Fly.Name = "Fly"
+Fly.Parent = ScrollingFrame
+Fly.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Fly.Position = UDim2.new(0.0182186235, 0, 0.0172198527, 0)
+Fly.Size = UDim2.new(0, 138, 0, 31)
+Fly.Font = Enum.Font.SourceSans
+Fly.Text = "سكريت طيران"
+Fly.TextColor3 = Color3.fromRGB(0, 0, 0)
+Fly.TextSize = 14.000
+Fly.MouseButton1Down:connect(function()
+	loadstring(game:HttpGet('https://pastebin.com/raw/YSL3xKYU'))()
+end)
 
-UICorner.Parent = TextButton
+UICorner.Parent = Fly
 
 UICorner_2.Parent = Frame
 
-TextButton_2.Parent = ScreenGui
-TextButton_2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-TextButton_2.Position = UDim2.new(0.0153846154, 0, 0.479999989, 0)
-TextButton_2.Size = UDim2.new(0, 200, 0, 50)
-TextButton_2.Font = Enum.Font.SourceSans
-TextButton_2.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextButton_2.TextSize = 14.000
-
-UICorner_3.Parent = TextButton_2
-
 -- Scripts:
 
-local function CFJYVB_fake_script() -- Frame.LocalScript 
+local function NAII_fake_script() -- Frame.LocalScript 
 	local script = Instance.new('LocalScript', Frame)
 
 	local UIS = game:GetService('UserInputService')
@@ -90,16 +80,4 @@ local function CFJYVB_fake_script() -- Frame.LocalScript
 		end
 	end)
 end
-coroutine.wrap(CFJYVB_fake_script)()
-local function RFFRGB_fake_script() -- ScreenGui.LocalScript 
-	local script = Instance.new('LocalScript', ScreenGui)
-
-	local SGui = script.Parent
-	local Frame = SGui:WaitForChild("Frame")
-	local Button = SGui:WaitForChild("TextButton")
-	
-	Button.MouseButton1Up:Connect(function()
-		Frame.Visible = not Frame.Visible
-	end)
-end
-coroutine.wrap(RFFRGB_fake_script)()
+coroutine.wrap(NAII_fake_script)()
